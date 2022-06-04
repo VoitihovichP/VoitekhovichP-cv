8a39e2e76b6ff9b3d7b8cd2cb212806bdcf73882
Pavel <pasha2003228@gmail.com>
2022-06-04 19:02:48 +0300

    feat: create react app and add eslint and prettier

---
+++ cv/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+} 
+
+export default App;
